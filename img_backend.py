3811e52637ab77ee9a78a1004f918d4f7108065a
Work Email <shedrack.aji@outlook.com>
2024-08-14 15:23:40 +0100

    update the backend FastApi url

--- img_backend.py
+++ img_backend.py
@@ -66,4 +66,4 @@ import os
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8000))
-    uvicorn.run("fastapi:app", host="0.0.0.0", port=port)
+    uvicorn.run("img_backend:app", host="0.0.0.0", port=port)

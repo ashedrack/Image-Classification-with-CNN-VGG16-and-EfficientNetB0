d863fa5793b378f0597fca8f9f4ad6c36703a4f8
Work Email <shedrack.aji@outlook.com>
2024-08-14 14:48:38 +0100

    update the backend FastApi url

--- fastapi.py
+++ fastapi.py
@@ -61,5 +61,9 @@ async def predict(file: UploadFile = File(...)):
 
 import uvicorn
 
+import uvicorn
+import os
+
 if __name__ == "__main__":
-    uvicorn.run("fastapi:app", host="0.0.0.0", port=8001, reload=True)
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run("app2:app", host="0.0.0.0", port=port)

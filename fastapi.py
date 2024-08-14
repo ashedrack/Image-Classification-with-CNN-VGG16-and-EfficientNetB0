a5483ae372e7aac16874d018bf07fec3679d825c
Work Email <shedrack.aji@outlook.com>
2024-08-14 14:57:10 +0100

    update the backend FastApi url

--- fastapi.py
+++ fastapi.py
@@ -66,4 +66,4 @@ import os
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8000))
-    uvicorn.run("app2:app", host="0.0.0.0", port=port)
+    uvicorn.run("fastapi:app", host="0.0.0.0", port=port)
